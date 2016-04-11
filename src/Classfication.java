04c003f34f961c6d8b66ed8c2e02209ba491bec0
lilelr <lilelr@163.com>
2016-04-11 19:01:54 +0800

    classfication

--- src/Classfication.java
+++ src/Classfication.java
@@ -7,6 +7,17 @@ import java.util.Map;
  */
 public class Classfication {
 
+
+    public static boolean isTheFourTypeJudgedByManage(String str){
+        return str.indexOf("04")==1||str.indexOf("05")==1||str.indexOf("06")==1||str.indexOf("07")==1
+                ||str.indexOf("08")==1||str.indexOf("09")==1||str.indexOf("10")==1||str.indexOf("11")==1
+                ||str.indexOf("12")==1||str.indexOf("13")==1;
+    }
+
+    public static boolean isTheFourTypeJudgedByType(String str){
+        return !(str.indexOf("1")==0||str.indexOf("2")==0||str.indexOf("3")==0||str.indexOf("4")==0);
+    }
+
     public static void main(String[] args) {
         String vehicleInfoPath="/Users/yuxiao/项目/expriment/result/vehicleResult.csv";
         String outputPath="/Users/yuxiao/项目/expriment/result/classfication.csv";
@@ -14,6 +25,7 @@ public class Classfication {
         Map<String,String> truckDataMap = new HashMap<String,String>();
         Map<String,String> dangerDataMap = new HashMap<String,String>();
         Map<String,String> maintainDataMap = new HashMap<String,String>();
+        Map<String,String> errorDataMap = new HashMap<>();
 
         try{
             File dataFile = new File(vehicleInfoPath);
@@ -34,14 +46,16 @@ public class Classfication {
                             manageArea += lineItems[i];
                         }
                     }
-                     if(manageCode.indexOf("01") == 1){
-                         guestDataMap.put(lineItems[0],lineItems[1]+","+lineItems[2]+","+manageArea);
-                     } else if(manageCode.indexOf("02") == 1){
-                         truckDataMap.put(lineItems[0],lineItems[1]+","+lineItems[2]+","+manageArea);
-                     } else if(manageCode.indexOf("03") == 1){
-                         dangerDataMap.put(lineItems[0],lineItems[1]+","+lineItems[2]+","+manageArea);
-                     } else if(manageCode.indexOf("04") == 1){
-                         maintainDataMap.put(lineItems[0],lineItems[1]+","+lineItems[2]+","+manageArea);
+                     if(manageCode.indexOf("01") == 1 && lineItems[1].indexOf("1") == 0){
+                         guestDataMap.put(lineItems[0],lineItems[1]+","+lineItems[2]+","+manageArea+",1");
+                     } else if(manageCode.indexOf("02") == 1 && lineItems[1].indexOf("2") == 0){
+                         truckDataMap.put(lineItems[0],lineItems[1]+","+lineItems[2]+","+manageArea+",2");
+                     } else if(manageCode.indexOf("03") == 1 && lineItems[1].indexOf("3") == 0){
+                         dangerDataMap.put(lineItems[0],lineItems[1]+","+lineItems[2]+","+manageArea+",3");
+                     } else if(isTheFourTypeJudgedByManage(manageCode) && isTheFourTypeJudgedByType(lineItems[1])){
+                         maintainDataMap.put(lineItems[0],lineItems[1]+","+lineItems[2]+","+manageArea+",4");
+                     } else{
+                         errorDataMap.put(lineItems[0],lineItems[1]+","+lineItems[2]+","+manageArea+",5");
                      }
 
                 }
@@ -53,7 +67,7 @@ public class Classfication {
             Writer writer = new OutputStreamWriter(new FileOutputStream(outFile),"UTF-8");
 
             for(Map.Entry<String,String> entry: guestDataMap.entrySet()){
-                writer.write(entry.getKey()+","+entry.getValue()+"\n");
+                writer.write(entry.getKey()+","+entry.getValue() +"\n");
             }
             for(Map.Entry<String,String> entry: truckDataMap.entrySet()){
                 writer.write(entry.getKey()+","+entry.getValue()+"\n");
@@ -62,6 +76,9 @@ public class Classfication {
                 writer.write(entry.getKey()+","+entry.getValue()+"\n");
             }
             for(Map.Entry<String,String> entry: maintainDataMap.entrySet()){
+                writer.write(entry.getKey()+","+entry.getValue()+"\n");
+            }
+            for(Map.Entry<String,String> entry: errorDataMap.entrySet()){
                 writer.write(entry.getKey()+","+entry.getValue()+"\n");
             }
             //            numsAndTypeMap.clear();
