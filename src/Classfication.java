c3c137967a46da2b9f81cfab0f7dc692941c7925
lilelr <lilelr@163.com>
2016-04-14 10:51:27 +0800

    classfication

--- src/Classfication.java
+++ src/Classfication.java
@@ -8,83 +8,122 @@ import java.util.Map;
 public class Classfication {
 
 
-    public static boolean isTheFourTypeJudgedByManage(String str){
-        return str.indexOf("04")==1||str.indexOf("05")==1||str.indexOf("06")==1||str.indexOf("07")==1
-                ||str.indexOf("08")==1||str.indexOf("09")==1||str.indexOf("10")==1||str.indexOf("11")==1
-                ||str.indexOf("12")==1||str.indexOf("13")==1;
+    public static int isTheFourTypeJudgedByManage(String manageCode) {
+        if (manageCode.indexOf("01") == 0) {
+            return 1;
+        } else if (manageCode.indexOf("02") == 0) {
+            return 2;
+        } else if (manageCode.indexOf("03") == 0) {
+            return 3;
+        } else {
+            return 4;
+        }
     }
 
-    public static boolean isTheFourTypeJudgedByType(String str){
-        return !(str.indexOf("1")==0||str.indexOf("2")==0||str.indexOf("3")==0||str.indexOf("4")==0);
+    public static int isTheFourTypeJudgedByType(String str) {
+        if (str.indexOf("1") == 0) {
+            return 1;
+        } else if (str.indexOf("2") == 0) {
+            return 2;
+        } else if (str.indexOf("3") == 0) {
+            return 3;
+        } else if(str.indexOf("999") ==0){
+            return 999;
+        } else{
+            return 4;
+        }
     }
 
     public static void main(String[] args) {
-        String vehicleInfoPath="/Users/yuxiao/项目/expriment/result/vehicleResult.csv";
-        String outputPath="/Users/yuxiao/项目/expriment/result/classfication.csv";
-        Map<String,String> guestDataMap = new HashMap<String,String>();
-        Map<String,String> truckDataMap = new HashMap<String,String>();
-        Map<String,String> dangerDataMap = new HashMap<String,String>();
-        Map<String,String> maintainDataMap = new HashMap<String,String>();
-        Map<String,String> errorDataMap = new HashMap<>();
+        String vehicleInfoPath = "/Users/yuxiao/项目/expriment/result/vehicleResult.csv";
+        String outputPath = "/Users/yuxiao/项目/expriment/result/classfication.csv";
+        Map<String, String> guestDataMap = new HashMap<String, String>();
+        Map<String, String> truckDataMap = new HashMap<String, String>();
+        Map<String, String> dangerDataMap = new HashMap<String, String>();
+        Map<String, String> maintainDataMap = new HashMap<String, String>();
+        Map<String, String> errorDataMap = new HashMap<>();
+        Map<String, String> typeErrorMap = new HashMap<>(); //不能分类的数据
+        int count=0;
+        int typeCount=0;
+        int typeErrorCount=0;
 
-        try{
+
+        int sumOfTheSameType = 0;
+        int sumOfDiffType = 0;
+        try {
             File dataFile = new File(vehicleInfoPath);
-            InputStreamReader readinput = new InputStreamReader(new FileInputStream(dataFile),"UTF-8");
+            InputStreamReader readinput = new InputStreamReader(new FileInputStream(dataFile), "UTF-8");
             BufferedReader reader = new BufferedReader(readinput);
             String line;
-//            int count=0;
-            while ((line=reader.readLine()) !=null){
-                String[] lineItems =line.split(",");
-//                System.out.println(new String(line.getBytes("GB2312"), "UTF-8"));
-                if(lineItems.length>=4){
-                     String manageCode = lineItems[3];
+            while ((line = reader.readLine()) != null) {
+                String[] lineItems = line.split(",");
+                //过滤江苏的车辆
+                if(lineItems[0].indexOf("苏") != 0) continue;
+
+                   count++;
+                if (lineItems.length >= 4) {
+                    typeCount++;
                     String manageArea = "";
+                    int[] manageCodeArr = new int[5];
                     for (int i = 3; i < lineItems.length; i++) {
-                        if (i != lineItems.length - 1) {
+                        if(i==3){
+                            String manageCode = lineItems[3];
+                            manageCode = manageCode.substring(1);
+                            manageCodeArr[isTheFourTypeJudgedByManage(manageCode)]++;
+                        } else{
+                            manageCodeArr[isTheFourTypeJudgedByManage(lineItems[i])]++;
+                        }
                             manageArea += lineItems[i] + ",";
-                        } else {
-                            manageArea += lineItems[i];
+                    }
+
+                    int maxManageCode =0;
+                    int maxManageCodePos =0;
+                    for(int i=1;i<=4;i++){
+                        if(manageCodeArr[i]>maxManageCode){
+                            maxManageCode = manageCodeArr[i];
+                            maxManageCodePos=i;
                         }
                     }
-                     if(manageCode.indexOf("01") == 1 && lineItems[1].indexOf("1") == 0){
-                         guestDataMap.put(lineItems[0],lineItems[1]+","+lineItems[2]+","+manageArea+",1");
-                     } else if(manageCode.indexOf("02") == 1 && lineItems[1].indexOf("2") == 0){
-                         truckDataMap.put(lineItems[0],lineItems[1]+","+lineItems[2]+","+manageArea+",2");
-                     } else if(manageCode.indexOf("03") == 1 && lineItems[1].indexOf("3") == 0){
-                         dangerDataMap.put(lineItems[0],lineItems[1]+","+lineItems[2]+","+manageArea+",3");
-                     } else if(isTheFourTypeJudgedByManage(manageCode) && isTheFourTypeJudgedByType(lineItems[1])){
-                         maintainDataMap.put(lineItems[0],lineItems[1]+","+lineItems[2]+","+manageArea+",4");
-                     } else{
-                         errorDataMap.put(lineItems[0],lineItems[1]+","+lineItems[2]+","+manageArea+",5");
-                     }
-
+//                    int typeSelf = isTheFourTypeJudgedByType(lineItems[1]);
+//                    String typeSelfStr ="";
+//                    if(typeSelf==9){
+//
+//                    }
+//
+             guestDataMap.put(lineItems[0], lineItems[1] + "," + isTheFourTypeJudgedByType(lineItems[1]) + "," + lineItems[2] + "," + manageArea +maxManageCodePos);
+                } else{
+                    typeErrorCount++;
+                    typeErrorMap.put(lineItems[0],line);
                 }
             }
+            System.out.println("江苏车辆总共数量："+ count);
+            System.out.println("能分类数量："+ typeCount);
+            System.out.println("不能分类数量："+ typeErrorCount);
             reader.close();
 
 
             File outFile = new File(outputPath);
-            Writer writer = new OutputStreamWriter(new FileOutputStream(outFile),"UTF-8");
+            Writer writer = new OutputStreamWriter(new FileOutputStream(outFile), "UTF-8");
 
-            for(Map.Entry<String,String> entry: guestDataMap.entrySet()){
-                writer.write(entry.getKey()+","+entry.getValue() +"\n");
+            for (Map.Entry<String, String> entry : guestDataMap.entrySet()) {
+                writer.write(entry.getKey() + "," + entry.getValue() + "\n");
             }
-            for(Map.Entry<String,String> entry: truckDataMap.entrySet()){
-                writer.write(entry.getKey()+","+entry.getValue()+"\n");
+            for (Map.Entry<String, String> entry : truckDataMap.entrySet()) {
+                writer.write(entry.getKey() + "," + entry.getValue() + "\n");
             }
-            for(Map.Entry<String,String> entry: dangerDataMap.entrySet()){
-                writer.write(entry.getKey()+","+entry.getValue()+"\n");
+            for (Map.Entry<String, String> entry : dangerDataMap.entrySet()) {
+                writer.write(entry.getKey() + "," + entry.getValue() + "\n");
             }
-            for(Map.Entry<String,String> entry: maintainDataMap.entrySet()){
-                writer.write(entry.getKey()+","+entry.getValue()+"\n");
+            for (Map.Entry<String, String> entry : maintainDataMap.entrySet()) {
+                writer.write(entry.getKey() + "," + entry.getValue() + "\n");
             }
-            for(Map.Entry<String,String> entry: errorDataMap.entrySet()){
-                writer.write(entry.getKey()+","+entry.getValue()+"\n");
+            for (Map.Entry<String, String> entry : errorDataMap.entrySet()) {
+                writer.write(entry.getKey() + "," + entry.getValue() + "\n");
             }
             //            numsAndTypeMap.clear();
             writer.close();
 
-        } catch (IOException e){
+        } catch (IOException e) {
             e.printStackTrace();
         }
 
